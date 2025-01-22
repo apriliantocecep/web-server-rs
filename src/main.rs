ddd033230596d0f1a0ed1d283425df1bc7eaa4ea
Cecep Aprilianto <cecepaprilianto@gmail.com>
2025-01-22 22:56:46 +0700

    feat: create & handle tcp connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io::{Read};
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Let's build web server");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    println!("Server started at port :7878");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream)
+            }
+            Err(e) => {
+                println!("Unable to connect: {}", e)
+            }
+        }
+    }
 }
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let mut request = String::new();
+
+    match stream.read(&mut buffer) {
+        Ok(size) => {
+            request.push_str(String::from_utf8_lossy(&buffer[..size]).as_ref());
+
+            println!("{}", request);
+        }
+        Err(e) => eprintln!("Unable to read stream: {}", e)
+    }
+}
+
