ef13323e836b0d61b70edbef7d4a3012c89c4191
Cecep Aprilianto <cecepaprilianto@gmail.com>
2025-01-24 10:37:45 +0700

    feat: handle multithreaded client request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use log::{error, info};
+use web_server::ThreadPool;
 
 const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE\r\nAccess-Control-Allow-Headers: Content-Type\r\n\r\n";
 const NOT_FOUND_RESPONSE: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
@@ -11,10 +12,14 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     info!("Server started at port :7878");
 
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                handle_client(stream)
+                pool.execute(|| {
+                    handle_client(stream);
+                })
             }
             Err(e) => {
                 error!("Unable to connect: {}", e)
@@ -44,7 +49,7 @@ fn handle_client(mut stream: TcpStream) {
 }
 
 fn handle_home_request(request: &str) -> (String, String) {
-    println!("Request: {}", request);
+    // println!("Request: {}", request);
     (OK_RESPONSE.to_string(), "Welcome".to_string())
 }
 
