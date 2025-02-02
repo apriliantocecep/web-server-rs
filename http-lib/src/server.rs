f6cb709923882c188463a5c8843c9645406580bf
Cecep Aprilianto <cecepaprilianto@gmail.com>
2025-02-02 09:34:41 +0700

    feat: add server struct & WIP impl start server

--- http-lib/src/server.rs
+++ http-lib/src/server.rs
@@ -1,6 +1,21 @@
+use std::net::{SocketAddr, TcpListener};
 use crate::http::HttpMethod;
+use std::io::Result;
 
 pub struct Route {
     http_method: HttpMethod,
     path: String,
+}
+
+pub struct Server {
+    addr: SocketAddr,
+}
+
+impl Server {
+    pub fn start(&self) -> Result<()> {
+        let address = self.addr;
+        let listener = TcpListener::bind(address)?;
+
+        Ok(())
+    }
 }
