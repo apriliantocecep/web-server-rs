c44f2c6f7fb12935b5951bdb3c9ff0c21a630005
Cecep Aprilianto <cecepaprilianto@gmail.com>
2025-02-03 09:24:13 +0700

    feat: add server listening info

--- http-lib/src/server.rs
+++ http-lib/src/server.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
 use std::net::{SocketAddr, TcpListener};
 use crate::http::HttpMethod;
 use std::io::Result;
+use log::info;
 
 pub struct Route {
     http_method: HttpMethod,
@@ -15,6 +17,7 @@ impl Server {
     pub fn start(&self) -> Result<()> {
         let address = self.addr;
         let listener = TcpListener::bind(address)?;
+        info!("Server listening on {}", address.to_string());
 
         Ok(())
     }
